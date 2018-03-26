25130c733a95c8a676009f38c7e27b1ef9af2a6e
Ferdous Azad <ferdousazad12@gmail.com>
2018-03-27 00:58:46 +0600

    Admin lte dependency added via bower

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/exchange/{coin_name}', 'ExchangeDataController@get_currency');
 Route::get('/exchange/{coin_name}/{convert_to}', 'ExchangeDataController@convert_currency');
 Route::get('/news', 'ExchangeDataController@latest_news');
 
+Route::get('admin', function () {
+    return view('admin_template');
+});
